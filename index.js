0f105226cc54ba5d42b03e3d956a0d0e582ee5b1
cardinn <username.lilrice@gmail.com>
2021-12-05 12:14:05 -0800

    asdasdasd

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const spawn = require("child_process");
-
+const f = require("figlet");
 const regex = /temp=([^'C]+)/;
-
+spawn.exec("vcgencmd measure_temp", (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 setTimeout(() => {
-  spawn.exec("vcgencmd measure_temp", (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
     const temp = regex.exec(stdout)[1];
-    console.log(temp);
+    f(temp, (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(data);
+    });
   });
 
 }, 1000);
